83594809b5fc59a20f2a847877b42241dd70bcac
wxujian <13777870719@163.com>
2018-03-21 18:34:22 +0800

    Sam
    加入了机房3D测试组件，JS文件内容还在处理

---
+++ src/views/pageJF.vue
@@ -0,0 +1,22 @@
+<template>
+    <div></div>
+</template>
+
+<script>
+  //  import svgThree from '@/utils/svg-three';
+  export default{
+    data() {
+      return {
+        msg: 'hello vue'
+      }
+    },
+    methods: {
+//      svgThree.drawSvg();
+    },
+    components: {}
+  }
+</script>
+
+<style>
+
+</style>
